be3d359435522f4d8eb0fa04bc0ba4987f4740fb
Abel Tshimbalanga <apexmachine2@gmail.com>
2021-09-02 18:57:21 +0200

    Add project files.

---
+++ toll_calculator/Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using CommercialRegistration;
+using ConsumerVehicleRegistration;
+using LiveryRegistration;
+
+namespace toll_calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class TollCalculator
+    {
+        public decimal CalculateToll(object vehicle) =>
+            vehicle switch
+            {
+                Car c => 2.00m,
+                Taxi t => 3.50m,
+                Bus b => 10.00m,
+                DeliveryTruck d => 10.00m,
+                { } => throw new ArgumentException(message: "Not a known vehicle type", paramName: nameof(vehicle)),
+                null => throw new ArgumentNullException(nameof(vehicle))
+            };
+        public decimal CalculateToll(object vehicle, bool occupancyPricing) =>
+            vehicle switch
+            {
+                //This below is how it will look if we didn't use the nested switch expression
+                /*
+                 Car { Passengers: 0 } => 2.00m + 0.50m,
+                Car { Passengers: 1 } => 2.0m,
+                Car { Passengers: 2 } => 2.0m - 0.50m,
+                Car c => 2.00m - 1.0m,
+
+                Taxi { Fares: 0 } => 3.50m + 1.00m,
+                Taxi { Fares: 1 } => 3.50m,
+                Taxi { Fares: 2 } => 3.50m - 0.50m,
+                Taxi t => 3.50m - 1.00m,
+
+                Bus b when ((double)b.Riders / (double)b.Capacity) < 0.50 => 5.00m + 2.00m,
+                Bus b when ((double)b.Riders / (double)b.Capacity) > 0.90 => 5.00m - 1.00m,
+                Bus b => 5.00m,
+
+                DeliveryTruck t when (t.GrossWeightClass > 5000) => 10.00m + 5.00m,
+                DeliveryTruck t when (t.GrossWeightClass < 3000) => 10.00m - 2.00m,
+                DeliveryTruck t => 10.00m,
+
+                { } => throw new ArgumentException(message: "Not a known vehicle type", paramName: nameof(vehicle)),
+                null => throw new ArgumentNullException(nameof(vehicle))
+                 */
+
+                Car c => c.Passengers switch
+                {
+                    0 => 2.00m + 0.5m,
+                    1 => 2.0m,
+                    2 => 2.0m - 0.5m,
+                    _ => 2.00m - 1.0m //here we use a discard, a placeholder that means it isn't really used but the syntax asks that it must be stated. It also acts as a short code for default
+                },
+
+                Taxi t => t.Fares switch
+                {
+                    0 => 3.50m + 1.00m,
+                    1 => 3.50m,
+                    2 => 3.50m - 0.50m,
+                    _ => 3.50m - 1.00m
+                },
+
+                Bus b when ((double)b.Riders / (double)b.Capacity) < 0.50 => 5.00m + 2.00m,
+                Bus b when ((double)b.Riders / (double)b.Capacity) > 0.90 => 5.00m - 1.00m,
+                Bus b => 5.00m,
+
+                DeliveryTruck t when (t.GrossWeightClass > 5000) => 10.00m + 5.00m,
+                DeliveryTruck t when (t.GrossWeightClass < 3000) => 10.00m - 2.00m,
+                DeliveryTruck t => 10.00m,
+
+                { } => throw new ArgumentException(message: "Not a known vehicle type", paramName: nameof(vehicle)),
+                null => throw new ArgumentNullException(nameof(vehicle))
+            };
+        public decimal PeakTimePremiumIfElse(DateTime timeOfToll, bool inbound)
+        {
+            //Alot of my code looks like this, whats the alternative
+            if ((timeOfToll.DayOfWeek == DayOfWeek.Saturday) ||
+                (timeOfToll.DayOfWeek == DayOfWeek.Sunday))
+            {
+                return 1.0m;
+            }
+            else
+            {
+                int hour = timeOfToll.Hour;
+                if (hour < 6)
+                {
+                    return 0.75m;
+                }
+                else if (hour < 10)
+                {
+                    if (inbound)
+                    {
+                        return 2.0m;
+                    }
+                    else
+                    {
+                        return 1.0m;
+                    }
+                }
+                else if (hour < 16)
+                {
+                    return 1.5m;
+                }
+                else if (hour < 20)
+                {
+                    if (inbound)
+                    {
+                        return 1.0m;
+                    }
+                    else
+                    {
+                        return 2.0m;
+                    }
+                }
+                else // Overnight
+                {
+                    return 0.75m;
+                }
+            }
+        }
+
+    }
+}
