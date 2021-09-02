ab982bd59bb80888b9ebaa866b20a79e1b08edf9
Abel Tshimbalanga <apexmachine2@gmail.com>
2021-09-02 20:26:08 +0200

    Read through and learn

--- toll_calculator/Program.cs
+++ toll_calculator/Program.cs
@@ -15,6 +15,7 @@ namespace toll_calculator
 
     public class TollCalculator
     {
+        #region CalculateToll
         public decimal CalculateToll(object vehicle) =>
             vehicle switch
             {
@@ -79,6 +80,15 @@ namespace toll_calculator
                 { } => throw new ArgumentException(message: "Not a known vehicle type", paramName: nameof(vehicle)),
                 null => throw new ArgumentNullException(nameof(vehicle))
             };
+
+        #endregion
+
+        /*
+         For the final feature, the toll authority wants to add time sensitive peak pricing. During the morning and evening rush hours, the tolls are doubled. 
+        That rule only affects traffic in one direction: inbound to the city in the morning, and outbound in the evening rush hour. During other times during the workday, 
+        tolls increase by 50%. Late night and early morning, tolls are reduced by 25%. During the weekend, it's the normal rate, regardless of the time. 
+        You could use a series of if and else statements to express this using the following code:
+        */
         public decimal PeakTimePremiumIfElse(DateTime timeOfToll, bool inbound)
         {
             //Alot of my code looks like this, whats the alternative
@@ -127,5 +137,101 @@ namespace toll_calculator
             }
         }
 
+        /*
+         The preceding code does work correctly, but isn't readable. You have to chain through all the input cases and the nested if statements to reason about the code.
+        Instead, you'll use pattern matching for this feature, but you'll integrate it with other techniques. 
+        You could build a single pattern match expression that would account for all the combinations of direction, day of the week, and time.
+        The result would be a complicated expression. It would be hard to read and difficult to understand. That makes it hard to ensure correctness. 
+        Instead, combine those methods to build a tuple of values that concisely describes all those states. 
+        Then use pattern matching to calculate a multiplier for the toll. The tuple contains three discrete conditions:
+
+        -The day is either a weekday or a weekend.
+        -The band of time when the toll is collected.
+        -The direction is into the city or out of the city
+
+        There are 16 different combinations of the three variables. By combining some of the conditions, you'll simplify the final switch expression.
+         */
+        #region Time
+        private static bool IsWeekDay(DateTime timeOfToll) =>
+            timeOfToll.DayOfWeek switch
+            {
+                DayOfWeek.Monday => true,
+                DayOfWeek.Tuesday => true,
+                DayOfWeek.Wednesday => true,
+                DayOfWeek.Thursday => true,
+                DayOfWeek.Friday => true,
+                DayOfWeek.Saturday => false,
+                DayOfWeek.Sunday => false,
+                _ => true
+            };
+        private static bool IsWeekDay(DateTime timeOfToll, bool therealme) =>
+              timeOfToll.DayOfWeek switch
+              {
+                  DayOfWeek.Saturday => false,
+                  DayOfWeek.Sunday => false,
+                  _ => true
+              };
+
+        private enum TimeBand
+        {
+            MorningRush,
+            Daytime,
+            EveningRush,
+            Overnight
+        }
+
+        private static TimeBand GetTimeBand(DateTime timeOfToll) =>
+            timeOfToll.Hour switch
+            {
+                < 6 or > 19 => TimeBand.Overnight,
+                < 10 => TimeBand.MorningRush,
+                < 16 => TimeBand.Daytime,
+                _ => TimeBand.EveningRush,
+            };
+        #endregion
+
+        public decimal PeakTimePremiumFull(DateTime timeOfToll, bool inbound) =>
+    (IsWeekDay(timeOfToll), GetTimeBand(timeOfToll), inbound) switch
+    {
+        (true, TimeBand.MorningRush, true) => 2.00m,
+        (true, TimeBand.MorningRush, false) => 1.00m,
+        (true, TimeBand.Daytime, true) => 1.50m,
+        (true, TimeBand.Daytime, false) => 1.50m,
+        (true, TimeBand.EveningRush, true) => 1.00m,
+        (true, TimeBand.EveningRush, false) => 2.00m,
+        (true, TimeBand.Overnight, true) => 0.75m,
+        (true, TimeBand.Overnight, false) => 0.75m,
+        (false, TimeBand.MorningRush, true) => 1.00m,
+        (false, TimeBand.MorningRush, false) => 1.00m,
+        (false, TimeBand.Daytime, true) => 1.00m,
+        (false, TimeBand.Daytime, false) => 1.00m,
+        (false, TimeBand.EveningRush, true) => 1.00m,
+        (false, TimeBand.EveningRush, false) => 1.00m,
+        (false, TimeBand.Overnight, true) => 1.00m,
+        (false, TimeBand.Overnight, false) => 1.00m,
+        _ => 1.00m
+    };
+        //the above cod, does not finnesse, too clunky
+        /*The above code works, but it can be simplified. All eight combinations for the weekend have the same toll. You can replace all eight with the following line:
+    (false, _, _) => 1.0m,
+
+    
+    Both inbound and outbound traffic have the same multiplier during the weekday daytime and overnight hours. Those four switch arms can be replaced with the following two lines
+    (true, TimeBand.Overnight, _) => 0.75m,
+(true, TimeBand.Daytime, _)   => 1.5m,
+
+    The code should look like the following code after those two changes:
+*/
+
+        public decimal PeakTimePremium(DateTime timeOfToll, bool inbound) =>
+    (IsWeekDay(timeOfToll), GetTimeBand(timeOfToll), inbound) switch
+    {
+        (true, TimeBand.MorningRush, true) => 2.00m,
+        (true, TimeBand.Daytime, _) => 1.50m,
+        (true, TimeBand.EveningRush, false) => 2.00m,
+        (true, TimeBand.Overnight, _) => 0.75m,
+        _ => 1.00m //this is cause everything else is 1.00
+    };
     }
+
 }
